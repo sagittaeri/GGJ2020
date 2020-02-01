e44e78e1cb545b6231ca6730f67b4ba2da34784f
jackwil93 <mail.jackwilson@gmail.com>
2020-02-01 11:29:11 +1100

    Ui now allows for inspecting two docs at the same time

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -8,15 +9,81 @@ public class GameController : MonoBehaviour
     public Transform selectedUI;
     public UIDropZone selectedUIDropZone;
 
+    public UIDocument inspected1;
+    public UIDocument inspected2;
+
+    RectTransform gameCanvasRect;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameCanvasRect = GameObject.Find("Canvas").GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (selectedUI != null) selectedUI.transform.position = Input.mousePosition;
+
+
+
+        // Zoom in / out
+        //if (inspect)
+        //{
+        //    transform.position = Vector3.Lerp(transform.position, inspectPos, Time.deltaTime / 10);
+        //    transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one, Time.deltaTime / 10);
+        //}
+        //else
+        //{
+        //    transform.position = Vector3.Lerp(transform.position, idlePos, Time.deltaTime / 10);
+        //    transform.localScale = Vector3.Lerp(transform.localScale, idleScale, Time.deltaTime / 10);
+        //}
+    }
+
+    public void TryInspectDoc(UIDocument uiDoc)
+    {
+        if (inspected1 == null)
+        {
+            InspectDoc1(uiDoc);
+        }
+        else if (inspected2 == null)
+        {
+            InspectDoc2(uiDoc);
+        }
+        else
+            return;
+    }
+
+    void InspectDoc1(UIDocument doc1)
+    {
+        RectTransform doc1Rect = doc1.GetComponent<RectTransform>();
+
+        doc1Rect.anchoredPosition = new Vector2(gameCanvasRect.sizeDelta.x / 4 * -1, 0);
+        doc1Rect.localScale = Vector3.one;
+
+        inspected1 = doc1;
+    }
+
+    void InspectDoc2(UIDocument doc2)
+    {
+        RectTransform doc2Rect = doc2.GetComponent<RectTransform>();
+
+        doc2Rect.anchoredPosition = new Vector2(gameCanvasRect.sizeDelta.x / 4, 0);
+        doc2Rect.localScale = Vector3.one;
+
+        inspected2 = doc2;
+    }
+
+    public void ExitInspect(UIDocument uiDoc)
+    {
+        uiDoc.GetComponent<RectTransform>().anchoredPosition = uiDoc.idlePos;
+        uiDoc.transform.localScale = uiDoc.idleScale;
+
+        if (uiDoc == inspected1)
+            inspected1 = null;
+
+        if (uiDoc == inspected2)
+            inspected2 = null;
+
     }
 }
--- Assets/Scripts/UIDocument.cs
+++ Assets/Scripts/UIDocument.cs
@@ -2,49 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UIDocument : MonoBehaviour, IPointerClickHandler, IPointerExitHandler
 {
-    public Vector3 inspectPos;
-    public Vector3 idlePos;
+    public Vector2 idlePos;
     public Vector3 idleScale;
 
-    bool inspect;
+    GameController GC;
+    Button closeButton;
+
+    public bool inspecting; // controlled by GameController
 
     public void Start()
     {
-        idlePos = transform.position;
+        GC = FindObjectOfType<GameController>();
+        closeButton = GetComponentInChildren<Button>();
+
+        closeButton.onClick.AddListener(CloseButton);
+
+
+        idlePos = GetComponent<RectTransform>().anchoredPosition;
         idleScale = transform.localScale;
+    }
+
+    public void CloseButton()
+    {
+        GC.ExitInspect(this);
     }
 
 
     public void Update()
     {
-        if (inspect)
-        {
-            transform.position = Vector3.Lerp(transform.position, inspectPos, Time.deltaTime/10);
-            transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one, Time.deltaTime/10);
-        }
-        else
-        {
-            transform.position = Vector3.Lerp(transform.position, idlePos, Time.deltaTime/10);
-            transform.localScale = Vector3.Lerp(transform.localScale, idleScale, Time.deltaTime/10);
-        }
+      
 
     }
 
 
     public void OnPointerClick(PointerEventData pointerEventData)
     {
-        transform.position = inspectPos;
-        transform.localScale = Vector3.one;
-        inspect = true;
+        GC.TryInspectDoc(this);
     }
 
     public void OnPointerExit (PointerEventData pointerEventData)
     {
-        transform.position = idlePos;
-        transform.localScale = idleScale;
-      //  inspect = false;
+    //
     }
 }
