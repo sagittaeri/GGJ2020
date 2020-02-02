c41ad647018226be6c66f3b818a0b0629c30f1f8
KG <sagittaeri@me.com>
2020-02-02 12:19:44 +1100

    main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class MainMenu : MonoBehaviour
+{
+    public CanvasGroup group;
+    public GameObject hideOnStart;
+    public Image background;
+    public Color initialTint;
+
+    public void StartGame()
+    {
+        group.interactable = false;
+        hideOnStart.SetActive(false);
+        background.DOColor(initialTint, 1f).OnComplete(() =>
+        {
+            SceneManager.LoadScene("Prototype", LoadSceneMode.Single);
+        });
+    }
+}
