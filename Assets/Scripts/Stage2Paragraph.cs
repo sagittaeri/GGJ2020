2988512b42749febdc4d4963aa58a716765b9040
jackwil93 <mail.jackwilson@gmail.com>
2020-02-02 13:00:38 +1100

    Fixed up stage 1a to make it playabale

---
+++ Assets/Scripts/Stage2Paragraph.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stage2Paragraph : MonoBehaviour
+{
+    Text myText;
+
+    [TextArea]
+    public string content;
+
+
+    public List<Stage2Choic> matchingChoices = new List<Stage2Choic>();
+
+
+
+}
